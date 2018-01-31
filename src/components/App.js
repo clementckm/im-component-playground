cba683806d41822cb4af53e39f4fd3339af79661
clement <chankinmingclement@gmail.com>
2018-01-31 00:41:15 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Button, Label, NextButton } from 'im-component-lib';
 import '../assets/stylesheets/base.scss';
 
 
 const App = ({ name }) => {
   return (
     <div>
-      <h1>Hello, {name}!</h1>
+      <Label>Basic Button</Label>
+      <br />
+
+      <Button>Default</Button>
+      <div>
+        <Button
+          bgColor="#13CE66"
+          size="small"
+        >
+          small
+        </Button>
+      </div>
+      <div>
+        <Button
+          bgColor="#FFC82C"
+          size="medium"
+        >
+          medium
+        </Button>
+      </div>
+      <div>
+        <Button
+          bgColor="#FF7849"
+          size="large"
+        >
+          large
+        </Button>
+      </div>
+      <div>
+        <Button
+          size="wide"
+        >
+          wide
+        </Button>
+      </div>
+      <div>
+        <Button
+          bgColor="#FF49DB"
+          size="extraWide"
+        >
+          extra wide
+        </Button>
+      </div>
+      <div>
+       <Button
+         bgColor="#7E5BEF"
+         size="fullWidth"
+       >
+         full width
+       </Button>
+      </div>
+      <Label>Persona Button</Label>
+       <NextButton
+        size="medium"
+        onClick={evt => evt }
+        label='Next >'
+       ></NextButton>
     </div>
   );
 };
